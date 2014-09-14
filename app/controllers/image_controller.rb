f61b079111755e14020a618695e34d2cd39fbf68
Vijay Dhama <vjdhama26@gmail.com>
2014-09-15 00:53:55 +0530

    Added index page content, some sass

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,28 +2,27 @@ class ImageController < ApplicationController
 	# This is to prevent csrf warning
   skip_before_filter :verify_authenticity_token, only: [:add_image]
 
-  def index
-  	@images = Image.all
-  end
   def uploader
 
   end
+
   def index
   	@images = Image.all
   	#render_template (:file => "#{Rails.root}/app/views/image/index")
   end
 
   def add_image
+    # url form http://p.imgci.com/db/PICTURES/CMS/193700/193739.jpg
 	image_url = params['url']
-	name = image_url.split("/").last.split(".").first  	
+	name = image_url.split("/").last.split(".").first
 	@u = Image.new(name: name, url: image_url)
 	p @u.save!
 	render nothing: true
 	# Run rails c and check for Image.all.last
   end
 
-  def show 
-  	#@images = Image.all
-
+  def show
+  	@images = Image.all
+    
   end
 end
